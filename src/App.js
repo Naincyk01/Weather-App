31260a2d0fba715a6c772f910c61b4f4c5fae3ae
Naincy <naincyk15042004@gmail.com>
2023-07-06 01:20:38 +0530

    Setting for the project and building the head.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {FaHourglassStart} from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-400 t0-blue-700 h-fit  shadow-gray-400 text-center font-bold text-2xl">
+      Header
+      
     </div>
   );
 }
