754322aed846ddf7706e6973824e257588a22556
Naincy <naincyk15042004@gmail.com>
2023-07-07 22:15:52 +0530

    Adding Top Buttons  to the Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
 import {FaHourglassStart} from "react-icons/fa";
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-400 t0-blue-700 h-fit  shadow-gray-400 text-center font-bold text-2xl">
-      Header
-      
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-300 to-blue-400  h-fit 
+    shadow-xl shadow-gray-400 text-center font-bold text-2xl">
+     <TopButtons/>
+     <Inputs/>
     </div>
   );
 }
