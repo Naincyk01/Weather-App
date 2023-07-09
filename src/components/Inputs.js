8b667245f656002d22337e0429cc5e81b30f0874
Naincy <naincyk15042004@gmail.com>
2023-07-09 22:56:49 +0530

    Addition of extra feature to header part

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 import { HiOutlineSearch } from "react-icons/hi";
-import { GrLocation } from "react-icons/gr";
+import { CiLocationOn } from "react-icons/ci";
 const Inputs = () => {
   return (
-    <div className='flex flex-row justify-center my-6'>
-      <div className='flex flex-row w-2/3 items-center justify-center space-x-4'>
-        <input 
-        type='text'
-         placeholder='Search for the city....'
-          className='text-xl font-light p-1 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'/>
+    <div className="flex flex-row justify-center my-4">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search for the city...."
+          className="text-xl font-light p-1 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+        />
 
-       <HiOutlineSearch/>
-       <GrLocation/>
+        <HiOutlineSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125 "
+        />
+        <CiLocationOn
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125 "
+        />
+      </div>
+
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button name="metric" className="text-xl text-white font-light">*C</button>
+        <p className="text-xl text-white mx-1">|</p>
+        <button name="imperial" className="text-xl text-white font-light">*F</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inputs
+export default Inputs;
